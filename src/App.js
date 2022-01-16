30d582173102515d8eaf71784457d3bb5cc0ac67
Bryan Mullen <bryan@bryanmullen.io>
2022-01-16 16:06:06 +0000

    Testing changes to main

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Hi Carrie</h1>
         <p>
-          Is it working
+          Apparently it's working now
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src="meme.jpeg" alt="meme"/>
       </header>
     </div>
   );
--- src/App.test.js
+++ src/App.test.js
@@ -3,12 +3,7 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Hi Carrie/i);
   expect(linkElement).toBeInTheDocument();
 });
 
-test('renders another learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
